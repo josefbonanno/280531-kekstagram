6a3f616f22ce4a97e8d4000e64c9506275229c8b
josefbonanno <evgbondar18@gmail.com>
2017-04-25 19:12:01 +0300

    последнее изменение

--- js/pictures.js
+++ js/pictures.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var picturesDescriptions = [];
-var commentsArr = [
+ var picturesDescriptions = [];
+ var commentsArr = [
  'Все отлично',
  'В целом всё неплохо. Но не всё',
  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце-концов это просто непрофессионально',
@@ -13,43 +13,83 @@ var getRandomLike = function(min, max) {
   return Math.floor(Math.random() * (max-min+1)) + min;
 };
 var getRandomComment = function() {
-  return Math.floor(Math.random() * commentsArr.length); /* тут получаем рандомный индекс*/
+  var indexArr = Math.floor(Math.random() * commentsArr.length);
+  return commentsArr[indexArr]; // тут получаем рандомный индекс
 }
 for (var i=0; i < 25; i++) {
   picturesDescriptions[i] = {
-  'url': 'photos/'+i+'.jpg',
-  'likes': getRandomLike(15, 200), /* преобразовать к промежутку*/
-  'comments': getRandomComment(),  /* преобразовать к промежутку*/
-  } 
-}
-
-console.log(picturesDescriptions);
-
-
-var picturesTemplate = document.getElementById('picture-template');
- for (var i=0; i < 25; i++) {
-  var clonedNode = picturesTemplate.clone(true);
-  clonedNode.querySelector('img').setAttribute('src', picturesDescriptions[i].url); /*заменяем src из img на url в объекте массива */
-  clonedNode.querySelector('.picture-comments').textContent(picturesDescriptions[i].comments); /*и так с likes и comments */
-  clonedNode.querySelector('.picture-likes').textContent(picturesDescriptions[i].likes);
+  'url': 'photos/'+(i+1)+'.jpg',
+  'likes': getRandomLike(15, 200), // преобразовать к промежутку
+  'comments': getRandomComment(),  // преобразовать к промежутку
+  }
 }
 
 
+
+
+var picturesTemplate = document.getElementById('picture-template').content;
 var fragment = document.createDocumentFragment();
-for (var i=0; i<25, i++) {
-  fragment.appendChild(picturesDescriptions[i]);
+ for (var i=0; i < 25; i++) {
+  var clonedNode = picturesTemplate.cloneNode(true);
+  clonedNode.querySelector('img').setAttribute('src', picturesDescriptions[i].url); //заменяем src из img на url в объекте массива
+  clonedNode.querySelector('.picture-comments').textContent = picturesDescriptions[i].comments.length; //и так с likes и comments
+  clonedNode.querySelector('.picture-likes').textContent = picturesDescriptions[i].likes;
+  fragment.appendChild(clonedNode);
 }
-div.pictures.appendChild(fragment);
 
+ document.querySelector('.pictures').appendChild(fragment);
 
+//4
 var hideOverlay = document.querySelector('.upload-overlay');
 hideOverlay.classList.add('invisible');
 
-
+// 5
 var showGalleryOverlay = document.querySelector('.gallery-overlay');
 showGalleryOverlay.classList.remove('invisible');
-for (var i=0; i < 25; i++) {
-   document.querySelector('.gallery-overlay-image').setAttribute('src', picturesDescriptions[i].url);
-   document.querySelector('.gallery-overlay-controls-like').textContent(picturesDescriptions[i].likes);
-   document.querySelector('.gallery-overlay-controls-comments').textContent(picturesDescriptions[i].comments);
+
+   document.querySelector('.gallery-overlay-image').setAttribute('src', picturesDescriptions[0].url);
+   document.querySelector('.gallery-overlay-controls-like').textContent = picturesDescriptions[0].likes;
+   document.querySelector('.gallery-overlay-controls-comments').textContent = picturesDescriptions[0].comments;
+
+
+
+// 2 задание
+
+/*picturesTemplate.addEvent Listener('click', functin(event) {
+  event.preventDefault();
+  showGalleryOverlay.classList.remove('invisible');
+}); */
+var openPictures = document.querySelector('.pictures');
+  openPictures.addEventListener('click', function(event) {
+   event.preventDefault();
+   var target = event.target;
+   if (target.className = 'picture') {
+   showGalleryOverlay.classList.remove('invisible');
+   }
+  });
+
+var closePicture = document.querySelector('.gallery-overlay-close');
+  closePicture.addEventListener('click', function(event) {
+  event.preventDefault();
+  showGalleryOverlay.classList.add('invisible');
+});
+
+document.addEventListener('keydown', function(event){
+  if (event.keyCode === 27) {
+  showGalleryOverlay.classList.add('invisible');
 }
+});
+/*
+var ENTER_KEY_CODE = 13;
+var isActivationEvent === ENTER_KEY_CODE;
+*/
+
+/*
+var hideUploadForm = document.querySelector('.upload-form-cancel');
+var uploadOverlay = document.querySelector('.upload-overlay');
+hideUploadForm.addEventListener('click', function (event) {
+  event.preventDefault();
+  uploadOverlay.classList.add('upload-overlay-close');
+});*/
+
+// 3 задание
